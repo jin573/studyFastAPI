d94762cb86762bfac45848039185d4d32d6a6ef1
JinSeon Kim <rlawlstjs5573@gmail.com>
2025-02-24 02:10:33 +0900

    라우팅 설정

--- api/main.py
+++ api/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from api.routers import task, done
 
 app = FastAPI() #FastAPI 인스턴스
 
-@app.get("/hello") #get 방식 경로 async로 비동기
+"""@app.get("/hello") #get 방식 경로 async로 비동기
 async def hello():
-    return {"message": "hello world!"}
+    return {"message": "hello world!"}"""
+
+
+app.include_router(task.router) #내장 메서드 include_router
+app.include_router(done.router)
