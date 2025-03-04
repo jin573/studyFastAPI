a049572ca20c96889c3275b4395625ec1ab35b0e
JinSeon Kim <rlawlstjs5573@gmail.com>
2025-03-04 22:29:30 +0900

    CRUD 비동기 처리

--- api/cruds/task.py
+++ api/cruds/task.py
@@ -2,20 +2,26 @@
 from sqlalchemy import select
 from sqlalchemy.engine import Result
 from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 import api.models.task as task_model
 import api.schemas.task as task_schema
 
-def create_task(db:Session, task_create:task_schema.TaskCreate) -> task_model.Task: #반환 타입 힌트
+#def create_task(db:Session, task_create:task_schema.TaskCreate) -> task_model.Task: #반환 타입 힌트
+async def create_task(
+    db:AsyncSession, task_create:task_schema.TaskCreate
+)->task_model.Task:
     task = task_model.Task(**task_create.dict())#**으로 키 값 가져오기
     db.add(task)
-    db.commit()
-    db.refresh(task)
+    #db.commit()
+    #db.refresh(task)
+    await db.commit() #여기에서는 비동기로 처리되면 안됨 -> await
+    await db.refresh(task)
     return task
 
 
-def get_tasks_with_done(db:Session) -> list[tuple[int, str, bool]]:
-    result:Result = db.execute(
+async def get_tasks_with_done(db:AsyncSession) -> list[tuple[int, str, bool]]:
+    result:Result = await db.execute(
         select(
             task_model.Task.id,
             task_model.Task.title,
@@ -27,19 +33,19 @@ def get_tasks_with_done(db:Session) -> list[tuple[int, str, bool]]:
 #result 객체는 여러 튜플로 반환된다.
 #하지만 select로는 Task라는 테이블 자체를 반환하게 되므로 
 #scalars로 값 자체를 반환
-def get_task(db:Session, task_id:int) -> task_model.Task|None:
-    result:Result = db.execute(
+async def get_task(db:AsyncSession, task_id:int) -> task_model.Task|None:
+    result:Result = await db.execute(
         select(task_model.Task).filter(task_model.Task.id==task_id)
     )
     return result.scalars().first()
 
-def update_task(db:Session, task_create:task_schema.TaskCreate, original:task_model.Task)->task_model.Task:
+async def update_task(db:AsyncSession, task_create:task_schema.TaskCreate, original:task_model.Task)->task_model.Task:
     original.title=task_create.title
     db.add(original)
-    db.commit()
-    db.refresh(original)
+    await db.commit()
+    await db.refresh(original)
     return original
 
-def delete_task(db:Session, original:task_model.Task) -> None:
-    db.delete(original)
-    db.commit()
+def delete_task(db:AsyncSession, original:task_model.Task) -> None:
+    await db.delete(original)
+    await db.commit()
--- api/routers/task.py
+++ api/routers/task.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 import api.cruds.task as task_crud
 import api.schemas.task as task_schema
@@ -12,23 +13,26 @@ async def list_tasks():
     pass
 """
 @router.get("/tasks", response_model=list[task_schema.Task])
-async def list_tasks(db:Session=Depends(get_db)):
-    return task_crud.get_tasks_with_done(db)
+async def list_tasks(db:AsyncSession=Depends(get_db)):
+    return await task_crud.get_tasks_with_done(db)
 
-@router.post("/tasks", response_model=task_schema.TaskCreateResponse)
+"""@router.post("/tasks", response_model=task_schema.TaskCreateResponse)
 async def create_task(task_body: task_schema.TaskCreate, db:Session = Depends(get_db)): #의존성 주입
-    return task_crud.create_task(db, task_body)
+    return task_crud.create_task(db, task_body)"""
+@router.post("/tasks", response_model=task_schema.TaskCreateResponse)
+async def create_task(task_body: task_schema.TaskCreate, db:AsyncSession = Depends(get_db)): #의존성 주입
+    return await task_crud.create_task(db, task_body) #task_crud의 create_task에서 await 사용하므로 라우팅시에도 await 사용하여야 함
 
 @router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
-async def update_task(task_id: int, task_body: task_schema.TaskCreate, db:Session=Depends(get_db)):
-    task=task_crud.get_task(db, task_id=task_id)
+async def update_task(task_id: int, task_body: task_schema.TaskCreate, db:AsyncSession=Depends(get_db)):
+    task=await.task_crud.get_task(db, task_id=task_id)
     if task is None:
         raise HTTPException(status_code=404, detail="Task not found") #예외를 발생
-    return task_crud.update_task(db, task_body, original=task)
+    return await task_crud.update_task(db, task_body, original=task)
 
 @router.delete("/tasks/{task_id}", response_model=None)
-async def delete_task(task_id: int, db:Session=Depends(get_db)):
-    task = task_crud.get_task(db, task_id=task_id)
+async def delete_task(task_id: int, db:AsyncSession=Depends(get_db)):
+    task = await task_crud.get_task(db, task_id=task_id)
     if task is None:
         raise HTTPException(status_code=404, detail="Task not found")
-    return task_crud.delete_task(db, original=task)
+    return await task_crud.delete_task(db, original=task)
